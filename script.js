32ea7ff709dc076545201e527aafeb280f8221aa
RyanT25 <ryan30thompson@outlook.com>
2024-11-07 14:00:34 +0000

    added upgrade 2 js and edited css

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ let numPerClick=1;
 // * Upgrade 1 --
 let numOfUpgrade1=0;
 let upgrade1Cost=15;
+// * Upgrade 2 --
+let numOfUpgrade2=0;
+let upgrade2Cost=150;
 
 let gameObject=document.getElementById('game-object');
 let scoreDisplay=document.getElementById('score');
@@ -11,6 +14,10 @@ let scoreDisplay=document.getElementById('score');
 let upgrade1=document.getElementById('upgrade1-button');
 let upgrade1Count=document.getElementById('num-of-upgrade1');
 let upgrade1CostUpdate=document.getElementById('upgrade1-cost');
+// * Upgrade 2 --
+let upgrade2=document.getElementById('upgrade2-button');
+let upgrade2Count=document.getElementById('num-of-upgrade2');
+let upgrade2CostUpdate=document.getElementById('upgrade2-cost');
 
 // ! Press Button Function
 function addScore(){
@@ -52,5 +59,40 @@ function buyUpgrade1(){
 function upgrade1CostIncrease(){
     upgrade1Cost=upgrade1Cost*1.25;
     upgrade1CostUpdate.innerHTML=Math.round(upgrade1Cost);
+};
+
+//  ! Upgrade 2 Function
+function addUpgrade2(){
+    buyUpgrade2();
+    numOfUpgrade2+=numPerClick;
+    upgrade2CostIncrease();
+    setInterval(function(){
+        Upgrade2AddScore()
+    }, 1000);
+    upgrade2Count.innerHTML=numOfUpgrade2;
+};
+upgrade2.addEventListener('click', addUpgrade2);
+
+// * Upgrade 2 Auto Click --
+function Upgrade2AddScore(){
+    num=num+(numOfUpgrade2*(numPerClick*1));
+    scoreDisplay.innerHTML=Math.trunc(num);
+};
+
+// * Upgrade 2 Purchase --
+function buyUpgrade2(){
+    if(num>=upgrade2Cost){
+        num=num-upgrade2Cost;
+    }
+    else{
+        addUpgrade2()=false;
+    };
+    scoreDisplay.innerHTML=Math.trunc(num);
+};
+
+// * Upgrade 2 Cost Increase --
+function upgrade2CostIncrease(){
+    upgrade2Cost=upgrade2Cost*1.25;
+    upgrade2CostUpdate.innerHTML=Math.round(upgrade2Cost);
 };
 
